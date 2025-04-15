b592b3db367a565efae8ac4aede56fcdcc82756b
darlene.escobar <darlene.escobar@utec.edu.pe>
2025-04-14 23:14:51 -0500

    hackaton 1_2025

---
+++ src/test/java/com/example/hackaton1_2025/Hackaton12025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackaton1_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton12025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
